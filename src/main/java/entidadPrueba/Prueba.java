1eb3522721ad9187aba9c57c0257c216e2360454
Luke-Heller <lucasivancardozo1@gmail.com>
2021-11-21 16:40:30 -0300

    Tercer commit, dni y nacimiento agg

--- src/main/java/entidadPrueba/Prueba.java
+++ src/main/java/entidadPrueba/Prueba.java
@@ -1,6 +1,10 @@
 package entidadPrueba;
 
+import java.util.Date;
+
 public class Prueba {
     private String nombre;
     private Integer edad;
+    private long dni;
+    private Date fechaNacimiento;
 }
