1d4fd96ffc80954073751b1d0059b54bec68f531
Luke-Heller <lucasivancardozo1@gmail.com>
2021-11-21 18:42:46 -0300

    "commit con getters y setters"

--- src/main/java/entidadPrueba/Prueba.java
+++ src/main/java/entidadPrueba/Prueba.java
@@ -7,4 +7,46 @@ public class Prueba {
     private Integer edad;
     private long dni;
     private Date fechaNacimiento;
+
+    public Prueba() {
+    }
+
+    public Prueba(String nombre, Integer edad, long dni, Date fechaNacimiento) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public long getDni() {
+        return dni;
+    }
+
+    public void setDni(long dni) {
+        this.dni = dni;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
